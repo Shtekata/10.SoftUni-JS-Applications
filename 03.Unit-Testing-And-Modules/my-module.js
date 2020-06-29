c632b92fc1d08781f6823af95fc31df9bc867d8c
Shtekata <gesheval@gmail.com>
2020-06-30 00:07:45 +0300

    01,02,03

---
+++ 03.Unit-Testing-And-Modules/my-module.js
@@ -0,0 +1,9 @@
+// 'use strict';
+
+// import * as myModule from './funcTest';
+
+// const a = funcTestModules.sum(5);
+// console.log(a);
+
+const sum = require('./funcTest');
+console.log(sum.sum(5));
---
+++ 03.Unit-Testing-And-Modules/project/app.js
@@ -0,0 +1,3 @@
+import { add } from './data';
+
+console.log(add(2, 3));
